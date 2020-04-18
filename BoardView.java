2b3e2b4b95a263acb171981b48e321b489102118
Jonathanthanthanthan <55176894+Jonathanthanthanthan@users.noreply.github.com>
2020-04-17 18:05:58 -0700

    Update BoardView.java
    Adds pits to board view and sets the labels for pits and mancalas

--- BoardView.java
+++ BoardView.java
@@ -1,5 +1,3 @@
-package View;
-
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -8,7 +6,6 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Shape;
 import java.util.ArrayList;
-
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -33,6 +30,12 @@ public class BoardView extends JPanel implements ChangeListener
 		this.setLayout(null);
 		this.player1Mancala = new MancalaPit();
 		this.player2Mancala = new MancalaPit();
+		pits = new Pit[12];
+		for(int i = 0;i < 12; i++)
+		{
+			pits[i] = new Pit();
+			this.add(pits[i]);
+		}
 	}
 
 	public void setBoardStyle(BoardStyler styler)
@@ -54,11 +57,47 @@ public class BoardView extends JPanel implements ChangeListener
 		player1Mancala.setBackground(styler.getPieceColor());
 		player2Mancala.setBackground(styler.getPieceColor());
 		
-		player1Mancala.setLabelText("A1");
-		player2Mancala.setLabelText("B1");
+		player1Mancala.setLabelText("B");
+		player2Mancala.setLabelText("A");
 		
 		player1Mancala.setLabelColor(styler.getLabelColor());
 		player2Mancala.setLabelColor(styler.getLabelColor());
+	}
+	
+		
+	public void updatePitPositions()
+	{
+		currentStyle.setPlayer1Pits();
+		currentStyle.setPlayer2Pits();
+		for(int i = 0; i < 6;i++)
+		{
+			pits[i].setLabelText("B"+(i+1));
+			pits[i].setLabelColor(currentStyle.getLabelColor());
+			pits[i].setShape(currentStyle.getPlayer1Pits()[i]);
+			pits[i].setBackground(currentStyle.getPieceColor());
+		}
+		
+		for(int i = 6; i < 12;i++)
+		{
+			pits[i].setLabelText("A"+(i-5));
+			pits[i].setLabelColor(currentStyle.getLabelColor());
+			pits[i].setShape(currentStyle.getPlayer2Pits()[i-6]);
+			pits[i].setBackground(currentStyle.getPieceColor());
+		}
+	}
+	
+	public void drawPitPositions(Graphics2D g2)
+	{
+		this.updatePitPositions();
+		for(int i = 0; i < 6;i++)
+		{
+			g2.draw(pits[i].getShape());
+		}
+		
+		for(int i = 6; i < 12;i++)
+		{
+			g2.draw(pits[i].getShape());
+		}
 	}
 
 	@Override
@@ -70,13 +109,13 @@ public class BoardView extends JPanel implements ChangeListener
 		//Gets the most recent information about Boards shape
 		this.player1Mancala.setShape(currentStyle.getPlayer1Mancala());
 		this.player2Mancala.setShape(currentStyle.getPlayer2Mancala());
-		
 		//Sets the outline thickness to whatever the styler calls for
 		g2.setStroke(new BasicStroke(currentStyle.getOutLineWidth()));
 		
 		//Draws the outlines of each Mancala panel
 		g2.draw(player1Mancala.getShape());
 		g2.draw(player2Mancala.getShape());
+		this.drawPitPositions(g2);
 	}
 
 
